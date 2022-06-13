b3ad355d75bb3881953076bff1841c05a22e9036
Stephen <stephenojo48@yahoo.com>
2022-06-13 17:15:49 +0100

    Update app.js
    JS projects update

--- setup/app.js
+++ setup/app.js
@@ -5,6 +5,29 @@ let count = 0;
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
-btns.forEach(function(btn) {
-	
-})
+btns.forEach(function (btn){
+	btn.addEventListener("click", function (e) {
+		// console.log(e.currentTarget);		//LOGS WHICH BUTTONS WERE CLICKED ON 
+		// console.log(e.currentTarget.classList);		//LOGS LISTS OF CLASSES THAT WERE CLICKED ON 
+		const styles = e.currentTarget.classList;
+		if(styles.contains("decrease")){
+			count--;
+		}
+		else if(styles.contains("increase")) {
+			count++;
+		}
+		else{
+			count = 0;
+		}
+		if (count > 0) {
+			value.style.color = "green";
+		}
+		if (count < 0) {
+			value.style.color = "red";
+		}
+		if (count === 0) {
+			value.style.color = "#222";
+		}
+		value.textContent = count;
+	});
+});
